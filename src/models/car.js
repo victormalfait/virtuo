5ced11d922eee43dae1645f0246cb46379044fc2
VIctor Malfait <victor.malfait@adopteunmec.com>
2019-05-27 22:18:53 +0200

    implement CRUD models

--- src/models/car.js
+++ src/models/car.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require("mongoose");
+const Promise = require("bluebird");
 
 class Car
 {
@@ -12,6 +13,59 @@ class Car
       available: Boolean
     });
     this.stationModel = model("Car", carSchema);
+  }
+
+  add({name, available}) {
+    const car = new this.stationModel({
+      name: name,
+      available: available
+    });
+    return new Promise((resolve, reject) => {
+      return car.save((err, car) => {
+        if (err) reject(err);
+        return resolve(car);
+      });
+    });
+  }
+
+  findById(idCar) {
+    return new Promise((resolve, reject) => {
+      return this.stationModel.findById(idCar, (err, car) => {
+        if (err) reject(err);
+        return resolve(car);
+      });
+    });
+  }
+
+  findAll() {
+    return new Promise((resolve, reject) => {
+      return this.stationModel.find({}, (err, cars) => {
+        if (err) reject(err);
+        return resolve(cars);
+      });
+    });
+  }
+
+  update({id, name, available}) {
+    return new Promise((resolve, reject) => {
+      return this.stationModel.findOneAndUpdate(
+        {_id: id},
+        {$set: {name: name, available: available}},
+        {new: true},
+        (err, car) => {
+          if (err) reject(err);
+          return resolve(car);
+        });
+    });
+  }
+
+  delete(idCar) {
+    return new Promise((resolve, reject) => {
+        this.stattionModel.deleteOne({id: idCar}, (err) => {
+          if (err) reject(err);
+          return resolve();
+        })
+    });
   }
 }
 
--- src/models/station.js
+++ src/models/station.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require("mongoose");
+const Promise = require("bluebird");
 
 class Station
 {
@@ -12,6 +13,58 @@ class Station
       cars: [{ type: Schema.Types.ObjectId, ref: 'Car' }]
     });
     this.stationModel = model("Sation", stationSchema);
+  }
+
+  add({name}) {
+    const station = new this.stationModel({
+      name: name
+    });
+    return new Promise((resolve, reject) => {
+      return station.save((err, station) => {
+        if (err) reject(err);
+        return resolve(station);
+      });
+    });
+  }
+
+  findById(idStation) {
+    return new Promise((resolve, reject) => {
+      return this.stationModel.findById(idStation, (err, station) => {
+        if (err) reject(err);
+        return resolve(station);
+      });
+    });
+  }
+
+  findAll() {
+    return new Promise((resolve, reject) => {
+      return this.stationModel.find({}, (err, stations) => {
+        if (err) reject(err);
+        return resolve(stations);
+      });
+    });
+  }
+
+  update({id, name, cars}) {
+    return new Promise((resolve, reject) => {
+      return this.stationModel.findOneAndUpdate(
+        {_id: id},
+        {$set: {name: name, cars: cars}},
+        {new: true},
+        (err, station) => {
+          if (err) reject(err);
+          return resolve(station);
+        });
+    });
+  }
+
+  delete(idStation) {
+    return new Promise((resolve, reject) => {
+        this.stationModel.deleteOne({id: idStation}, (err) => {
+          if (err) reject(err);
+          return resolve();
+        })
+    });
   }
 }
 
