c2233f88f3fbc05356087665b047140348a47d91
VIctor Malfait <victor.malfait@adopteunmec.com>
2019-05-27 21:26:58 +0200

    config to connect mongodb

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ const mongoose = require("mongoose");
 const app = express();
 const port = 8080;
 
+const dev_db_url = "mongodb://localhost:27017/virtuo";
+const options = {
+  useNewUrlParser: true
+};
+
+mongoose.connect(dev_db_url, options);
+mongoose.Promise = global.Promise;
+
+let db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Origin', '*');
